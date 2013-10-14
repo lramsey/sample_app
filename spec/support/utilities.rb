27530ed65435d3ee8ea8dca4bed2d825ef245f24
lramsey <lramsey177@gmail.com>
2013-10-13 19:16:58 -0700

    some ch. 8 exercises

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,32 @@
 include ApplicationHelper
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_no_error_message do
+  match do |page|
+    expect(page). to have_no_selector('div.alert.alert-error')
+  end
+end
+
+def valid_signup
+  fill_in "Name",         with: "Example User"
+  fill_in "Email",        with: "user@example.com"
+  fill_in "Password",     with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-success', text: message)
+  end
+end
